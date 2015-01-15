0af514d208b9efef94842d4e38dc20cd1518fef9
paybackman <travis.plummer@hp.com>
2015-01-15 11:45:47 -0600

    Initial commit

---
+++ CI-HelloWorld/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CI_HelloWorld.Startup))]
+namespace CI_HelloWorld
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
