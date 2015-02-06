31b03dae69b3fc5085e15a6c6117661c85407c9c
Travis Plummer <travisplummer217@hotmail.com>
2015-02-06 15:08:03 -0600

    Update HomeController.cs

--- CI-HelloWorld/Controllers/HomeController.cs
+++ CI-HelloWorld/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace CI_HelloWorld.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact pooage.";
 
             return View();
         }
