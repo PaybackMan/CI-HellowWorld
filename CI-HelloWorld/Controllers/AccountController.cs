a92ed3fabe327eefa576536069410885e11183fa
Travis Plummer <travisplummer217@hotmail.com>
2015-02-06 13:53:37 -0600

    Update AccountController.cs
    t

--- CI-HelloWorld/Controllers/AccountController.cs
+++ CI-HelloWorld/Controllers/AccountController.cs
@@ -72,7 +72,7 @@ namespace CI_HelloWorld.Controllers
             }
 
             // This doesn't count login failures tow 
-            // To enable password failures to trigger account lockout, change to shouldLockout: true
+            // To enable password failures to trigger accou  nt lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
